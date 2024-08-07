d79a168f396aba16768e94e0feeb6e7e52ed304e
Rafel <rdalmau23@ilg.cat>
2024-08-07 19:36:17 +0200

    hoy

--- proyecto_refugio/src/App.tsx
+++ proyecto_refugio/src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import Filtros from './components/Filtros';
+import Footer from './components/Footer';
 
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
       <header className="bg-indigo-500 text-white p-6 shadow-md">
-        <h1 className="text-4xl font-bold">Refugios</h1>
+        <h1 className="text-4xl font-bold mb-2">PetFinder</h1>
+
+        <div className="flex space-x-4">
+      <a href="#home" className="hover:text-white-400">Inicio</a>
+      <a href="#about" className="hover:text-white-400">Sobre Nosotros</a>
+      <a href="#services" className="hover:text-white-400">Colabora</a>
+      <a href="#contact" className="hover:text-white-400">Contacto</a>
+    </div>
       </header>
-      <main className="flex flex-1">
+      <main className="flex flex-1 justify-center items-center">
         <Filtros />
 
       </main>
+
+      <Footer />
     </div>
   );
 }
---
+++ proyecto_refugio/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Footer: React.FC = () => {
+
+
+  return (
+    <footer className="bg-indigo-500 text-white py-6">
+  <div className="container mx-auto flex justify-between items-center">
+    <div className="text-center">
+      <p className="text-sm">&copy; 2024 Pet Finder. Todos los derechos reservados.</p>
+    </div>
+   
+  </div>
+</footer>
+
+  );
+}
+
+export default Footer;
