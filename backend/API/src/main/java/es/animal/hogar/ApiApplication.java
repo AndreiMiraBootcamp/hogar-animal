d67f838b305dfdcd1d7f5ba51b98f1bd6247ac65
MihaiRomaniuc <romaniuc.mihai.mrm@gmail.com>
2024-08-08 19:49:25 +0200

    version 1

--- backend/API/src/main/java/es/animal/hogar/ApiApplication.java
+++ backend/API/src/main/java/es/animal/hogar/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package es.animal.hogar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/API/src/test/java/es/animal/hogar/ApiApplicationTests.java
+++ backend/API/src/test/java/es/animal/hogar/ApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package es.animal.hogar;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
