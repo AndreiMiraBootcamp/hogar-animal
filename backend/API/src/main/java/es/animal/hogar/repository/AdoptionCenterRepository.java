c5db66e09d6f985d8a21df52629974b12ee6a96a
MihaiRomaniuc <romaniuc.mihai.mrm@gmail.com>
2024-08-23 15:19:31 +0200

    FrontEnd Update

--- backend/API/src/main/java/es/animal/hogar/repository/AdoptionCenterRepository.java
+++ backend/API/src/main/java/es/animal/hogar/repository/AdoptionCenterRepository.java
@@ -1,6 +1,8 @@
 package es.animal.hogar.repository;
 
 import es.animal.hogar.entities.AdoptionCenter;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,10 @@ import java.util.List;
 
 @Repository
 public interface AdoptionCenterRepository extends JpaRepository<AdoptionCenter, Integer> {
-    List<AdoptionCenter> findByCityCityId(Integer cityId);
-    List<AdoptionCenter> findByUserUserId(Integer userId);
+	
+	@EntityGraph(attributePaths = {"city", "user"})
+	List<AdoptionCenter> findByCityCityId(Integer cityId);
+
+	@EntityGraph(attributePaths = {"city", "user"})
+	List<AdoptionCenter> findByUserUserId(Integer userId);
 }
